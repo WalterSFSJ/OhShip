d10d4333418e62401a2ff22db3b30fc76ad1e15d
SergiNaranjo <sergi.naranjo@enti.cat>
2025-10-01 13:29:05 +0200

    Main menu done, settings in progress

--- OhShip/Assets/Scripts/MainMenu.cs
+++ OhShip/Assets/Scripts/MainMenu.cs
@@ -3,14 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject settings;
     public void Play()
     {
         SceneManager.LoadScene("SergiTesting");
     }
 
-    void Settings()
+    public void Settings()
     {
-
+        mainMenu.SetActive(false);
+        settings.SetActive(true);
     }
 
     public void Exit()
